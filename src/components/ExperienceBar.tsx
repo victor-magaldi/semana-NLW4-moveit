eaea5768c20bf11e482ee73d643e94822b76ab08
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-02-23 21:01:17 -0300

    aplicando css modules na Experience Bar

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,13 +1,17 @@
-export default function ExperienceBar(){
-   return(
-      <header className="experience-bar"> 
-         <span>0 xp</span>
-         <div>
-            <div style={{width:"60%"}}></div>
+import styles from "../styles/components/ExperienceBar.module.css";
 
-            <span className="current-experience"  style={{left:"60%"}}>600px</span>
-         </div>
-         <span>600 xp</span>
-      </header>
-   )
+export default function ExperienceBar() {
+  return (
+    <header className={styles.experienceBar}>
+      <span>0 xp</span>
+      <div>
+        <div style={{ width: "60%" }}></div>
+
+        <span className={styles.currentExperience} style={{ left: "60%" }}>
+          600px
+        </span>
+      </div>
+      <span>600 xp</span>
+    </header>
+  );
 }
