e44a11ecd2f8813faa78fb8920e2a2bebacab977
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-02-23 21:25:18 -0300

    criar o componente profile e copia assets icons

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from "../styles/components/Profile.module.css";
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/victor-magaldi.png" alt="" />
+      <div>
+        <strong>Victor de Souza Magaldi</strong>
+        <p>
+          <img src="icons/level.svg" alt="level" />
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+}
