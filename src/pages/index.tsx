04a4df3f7322409c726a031521742400c6b38f8a
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-02-24 20:37:02 -0300

    cria componente Countdown e ajusta title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,24 @@
 import CompleteChallenges from "../components/CompleteChallenges";
+import Countdown from "../components/Countdown";
 import ExperienceBar from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+
+import Head from "next/head";
 
 import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>Moveit</Head>
+
       <ExperienceBar />
 
       <section>
         <div>
           <Profile></Profile>
           <CompleteChallenges></CompleteChallenges>
+          <Countdown></Countdown>
         </div>
         <div></div>
       </section>
