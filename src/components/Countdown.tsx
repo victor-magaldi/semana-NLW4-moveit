c48d3becf19efb983edcab269ea441975301dcd7
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-02-25 19:42:02 -0300

    cria botões com render condicional

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -3,7 +3,7 @@ import styles from "../styles/components/Countdown.module.css";
 
 export default function Countdown() {
   const [time, setTime] = useState(25 * 60);
-  const [active, setActive] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
@@ -12,17 +12,23 @@ export default function Countdown() {
   const [secondLeft, secondRight] = String(seconds).padStart(2, "0").split("");
 
   function startCountdown() {
-    setActive(!active);
+    setIsActive(true);
   }
+  function resetCountdonw() {
+    setIsActive(false);
+  }
+
   // toda vez que o estado active mudar ele vai executar a primeira funçao do parametro 1
   // colocando o time como segundo paramentro gerou uma recursividade
   useEffect(() => {
-    if (active && time > 0) {
+    if (isActive && time > 0) {
       setTimeout(() => {
         setTime(time - 1);
       }, 1000);
+    } else {
+      setTime(25 * 60);
     }
-  }, [active, time]);
+  }, [isActive, time]);
 
   return (
     <div>
@@ -37,13 +43,24 @@ export default function Countdown() {
           <span>{secondRight}</span>
         </div>
       </div>
-      <button
-        type="button"
-        className={styles.startCountdonw}
-        onClick={startCountdown}
-      >
-        Iniciar Ciclo
-      </button>
+
+      {!isActive ? (
+        <button
+          type="button"
+          className={styles.countdownButton}
+          onClick={startCountdown}
+        >
+          Iniciar Ciclo
+        </button>
+      ) : (
+        <button
+          type="button"
+          className={`${styles.countdownButton} ${styles.countdownButtonPause}`}
+          onClick={resetCountdonw}
+        >
+          Terminar Ciclo
+        </button>
+      )}
     </div>
   );
 }
